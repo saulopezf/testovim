595c5626172ac4fc6f4d5d7ea787a019e8568e59
saulopezf <saulopezf@proton.me>
2026-01-06 21:40:17 +0100

    choose one formatter for js and ts

--- lua/configs/formatter.lua
+++ lua/configs/formatter.lua
@@ -8,8 +8,39 @@ conform.setup {
     -- Define formatters per filetype
     formatters_by_ft = {
         lua = { "stylua" },
-        javascript = { "prettier", "eslint_d" },
-        typescript = { "prettier", "eslint_d" },
+
+        javascript = function(bufnr)
+            if conform.get_formatter_info("prettier", bufnr).available then
+                return { "prettier" }
+            else
+                return { "eslint_d" }
+            end
+        end,
+
+        typescript = function(bufnr)
+            if conform.get_formatter_info("prettier", bufnr).available then
+                return { "prettier" }
+            else
+                return { "eslint_d" }
+            end
+        end,
+
+        javascriptreact = function(bufnr)
+            if conform.get_formatter_info("prettier", bufnr).available then
+                return { "prettier" }
+            else
+                return { "eslint_d" }
+            end
+        end,
+
+        typescriptreact = function(bufnr)
+            if conform.get_formatter_info("prettier", bufnr).available then
+                return { "prettier" }
+            else
+                return { "eslint_d" }
+            end
+        end,
+
         css = { "prettier" },
         html = { "prettier" },
         -- Add more filetypes as needed
